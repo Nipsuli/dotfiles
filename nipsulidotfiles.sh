4046f22e02335c870ea5b9a3f095a4b6d190020c
Nipsuli <n.p.ahonen@gmail.com>
2024-04-03 10:09:12 +0300

    improve node install

--- nipsulidotfiles.sh
+++ nipsulidotfiles.sh
@@ -452,6 +452,11 @@ nipsulidotfiles::install_node() {
   # Going back to nvm
   # shellcheck disable=SC2312
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
   nvm install 20
   nvm alias default 20
 
